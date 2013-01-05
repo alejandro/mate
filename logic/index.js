dc5c1d9cb165f6036741b5eb794aa4aede69304d
Alejandro Morales <vamg008@gmail.com>
2013-01-04 23:28:40 -0600

    [0.0.1] Clean logic/index.js

--- logic/index.js
+++ logic/index.js
@@ -1,26 +1,9 @@
 module.exports = function (server, app){'use strict';
+
 var sio = require('socket.io')
-
-var io = sio.listen(server)
-
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  setInterval(function (){
-    socket.emit('heartbeat', +new Date)
-  }, 2500)
-
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-  socket.on('error', function (data){
-    console.log('ERROR', data)
-  })
-  socket.on('touchstart', console.log)
-  socket.on('deviceready', function (){
-    console.log('deviceready\n')
-    console.dir(arguments)
-  })
-});
+  , io = sio.listen(server)
+  , draw = io.of('/draw')
+  , chat = io.of('/chat')
 
 
 }
